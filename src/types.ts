301a36b8780368b2a32d86eff22123f6661bfed2
Mike Donnalley <mdonnalley@salesforce.com>
2022-08-30 09:12:01 -0600

    feat: make plugin generator 3PP friendly (#104)
    * feat: make plugin generator 3PP friendly
    
    * fix: remove .github for 3PP
    
    * feat: udpate topics when command is generated
    
    * chore: QA
    
    * chore: update nyc override
    
    * fix: update nyc config
    
    * fix: remove extends from nycrc

--- src/types.ts
+++ src/types.ts
@@ -5,18 +5,43 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-export interface PackageJson {
+export type Topic = {
+  description?: string;
+  external?: boolean;
+  subtopics: Topic;
+};
+
+export type NYC = {
+  extends: string;
+  lines: number;
+  statements: number;
+  branches: number;
+  functions: number;
+};
+
+export type PackageJson = {
   name: string;
   devDependencies: Record<string, string>;
   dependencies: Record<string, string>;
+  files: string[];
   oclif: {
     bin: string;
     dirname: string;
     hooks: Record<string, string | string[]>;
+    topics: Record<string, Topic>;
   };
   repository: string;
   homepage: string;
-}
+  bugs: string;
+  author: string;
+  description: string;
+  scripts: {
+    posttest: string;
+    'test:command-reference': string;
+    'test:deprecation-policy': string;
+    'test:json-schema': string;
+  };
+};
 
 export enum Hook {
   'sf:env:list' = 'sf env list',
