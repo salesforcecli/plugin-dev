0309dfaa5ba739b06abe9279b6141128ff8d7848
mshanemc <shane.mclaughlin@salesforce.com>
2022-11-14 12:59:42 -0600

    feat: converts message.json files to md

---
+++ test/commands/dev/convert/messages.nut.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import * as fs from 'fs';
+import * as path from 'path';
+import { execCmd } from '@salesforce/cli-plugins-testkit';
+import { expect } from 'chai';
+
+describe('dev convert messsages NUTs', () => {
+  const parentPath = path.join('test', 'commands', 'dev', 'convert', 'samples');
+  const resultPath = path.join(parentPath, 'basic.md');
+
+  after(async () => {
+    await fs.promises.rm(resultPath);
+  });
+
+  it('converts a basic file', async () => {
+    execCmd(`dev:convert:messages -f ${path.join(parentPath, 'basic.json')}`, { ensureExitCode: 0 });
+    expect(fs.existsSync(resultPath)).to.be.true;
+
+    // trim to control for EOL differences
+    const result = (await fs.promises.readFile(resultPath, 'utf8')).trim();
+    const expected = (await fs.promises.readFile(path.join(parentPath, 'expected.md'), 'utf8')).trim();
+    expect(result).to.equal(expected);
+  });
+});
---
+++ test/commands/dev/convert/samples/expected.md
@@ -0,0 +1,35 @@
+# string
+
+foo
+
+# stringTrimmed
+
+foo
+
+# multiline
+
+foo
+
+bar
+
+# array
+
+- boof
+
+- bark
+
+# object.bink
+
+bunk
+
+# nested.chicken.egg
+
+chicken
+
+# nested.chicken.chicken
+
+feather
+
+# nested.dog.blanket
+
+dog
