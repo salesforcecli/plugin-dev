b2fce8dcbcff3af93e7976af5356ff3fd1c36a02
Mike Donnalley <mdonnalley@salesforce.com>
2024-03-25 13:43:22 -0600

    chore: format

--- src/commands/dev/generate/command.ts
+++ src/commands/dev/generate/command.ts
@@ -5,12 +5,11 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-
 import { Messages } from '@salesforce/core';
 import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
 import { fileExists, generate } from '../../../util.js';
 
-Messages.importMessagesDirectoryFromMetaUrl(import.meta.url)
+Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
 const messages = Messages.loadMessages('@salesforce/plugin-dev', 'dev.generate.command');
 
 export default class GenerateCommand extends SfCommand<void> {
