bbe8965324409338ebb5ae09db7e7d9ea8fadbcd
Jamie Feingold <jfeingold@salesforce.com>
2026-08-21 09:12:30 -0500

    fix: rejiggered eslint.config.mjs (W-23928423)

--- src/commands/dev/audit/messages.ts
+++ src/commands/dev/audit/messages.ts
@@ -1,8 +1,17 @@
 /*
- * Copyright (c) 2022, salesforce.com, inc.
- * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ * Copyright 2025, Salesforce, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 import fs from 'node:fs';
 import { join, parse, relative, resolve } from 'node:path';
@@ -225,7 +234,7 @@ export default class AuditMessages extends SfCommand<AuditResults> {
       const bundleName = parse(bundleFileName).name;
       this.graph.addNode(bundleName, { type: 'bundle', name: bundleFileName, x: 1, y: 1 });
       // add the messages nodes and edges to the graph
-      /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
+
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       const keys: string[] = [...bundle.messages.keys()] as string[];
